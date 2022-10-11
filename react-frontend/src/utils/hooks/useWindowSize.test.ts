d877b273c7472dbe3784fe6a583c5d0e7bf1b721
Marc Wren <things@soul-fresh.com>
2022-10-10 23:44:49 -0400

    chore: lint fixes

--- react-frontend/src/utils/hooks/useWindowSize.test.ts
+++ react-frontend/src/utils/hooks/useWindowSize.test.ts
@@ -1,5 +1,8 @@
 // import { useWindowSize } from './useWindowSize';
 
+// TEMP required to pass linting. Remove once the test is implemented.
+export {}
+
 describe('useWindowSize', () => {
   xit('should be able to determine the current window size.', () => {});
 })
